e1e4792d2412c74822a989844ba3441107f6e526
alinecamardev <alinecamargospinto@gmail.com>
2025-03-28 20:51:53 -0300

    Descrição menu

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Sistema CALC");
+Console.WriteLine("Selecione uma opção:");
+Console.WriteLine("1 - Soma\n2 - Subtração\n3 - Divisão\n4 - Multiplicação\n5 - Potência\n6 - Raiz\n7 - Sair");
+
+Menu opcao = (Menu)int.Parse(Console.ReadLine());
+
+Console.ReadLine();
+
+enum Menu
+{
+    Soma = 1,
+    Subtracao,
+    Divisao,
+    Multiplicacao,
+    Potencia,
+    Raiz,
+    Sair
+}
+
